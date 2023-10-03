f5df56786dab5bb0c22ace138ea249235edb51e8
david <daviidgt03@gmail.com>
2023-10-03 17:29:40 +0200

    First commit & first steps done

---
+++ src/GamePanel.java
@@ -0,0 +1,24 @@
+import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+
+public class GamePanel extends JPanel implements ActionListener{
+  
+  GamePanel(){
+
+  }
+
+  public void startGame(){
+
+  }
+
+  public void paintComponent(Graphics g){
+
+  }
+public void draw (Graphics g){
+
+
+}
+  
+}
