b204239f6aa98adcc3ad2a201a826774e6a90be9
david <daviidgt03@gmail.com>
2023-10-04 16:02:39 +0200

    Basic functionality done

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,15 +1,7 @@
-import javax.swing.JFrame;
-
 public class SnakeGame {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Snake Game");
-        GamePanel gamePanel = new GamePanel();
 
-        frame.add(gamePanel);
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-
-        gamePanel.startGame();
-    }
+	public static void main(String[] args) {
+		
+		 new GameFrame();
+	}
 }
